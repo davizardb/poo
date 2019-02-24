d1f82bd652fcca428476e1c85605ed82045d84f8
davizardb <davi.luis@aluno.uece.br>
2019-02-24 20:40:47 -0300

    .

--- Main.cpp
+++ Main.cpp
@@ -6,7 +6,8 @@ int main()
 {
   cout << "quantas notas quer computar\n";
   cin << n;
-  NotaFiscal notas1[];
+  NotaFiscal notas1[]= new NotaFiscal*n;
+  
   float somaTotal;
   for (int i=0; i < 2)
   return 0;
